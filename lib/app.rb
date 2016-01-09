ebfe29e61d68b3a612f9f31eea487839f29080dd
Mitul Manish <mitul.manish@gmail.com>
2016-01-09 15:57:42 +1100

    Added changes according to review

--- lib/app.rb
+++ lib/app.rb
@@ -1,26 +1,24 @@
 require 'json'
-PRODUCT = "                     _            _       " +"\n"+
-    "                    | |          | |      " +"\n"+
-    " _ __  _ __ ___   __| |_   _  ___| |_ ___ " +"\n"+
-    "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|" +"\n"+
-    "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\" +"\n"+
-    "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/" +"\n"+
-    "| |                                       "+"\n"+
-    "|_|                                       " +"\n"
 
-BRAND = " _                         _     "+"\n"+
-"| |                       | |    "+"\n"+
-"| |__  _ __ __ _ _ __   __| |___ "+"\n"+
-"| '_ \\| '__/ _` | '_ \\ / _` / __|"+"\n"+
-"| |_) | | | (_| | | | | (_| \\__ \\"+"\n"+
-"|_.__/|_|  \\__,_|_| |_|  \\__,_|___/"+"\n"
+def brand
+  "       BRANDS"
+end
 
+def product
+  "       PRODUCTS"
+end
+def sales_report
+  "       SALES REPORT"
+end
 
-$products_hash = {}
-$report_file = nil
-$brands = []
+def file_setup
+  $products_hash = {}
+  $report_file = nil
+  $brands = []
+end
 
 def setup
+  file_setup
   path = File.join(File.dirname(__FILE__), '../data/products.json')
   file = File.read(path)
   $products_hash = JSON.parse(file)
@@ -29,17 +27,32 @@ def setup
   padding
 end
 
+def print_product_report
+  print_header product
+  padding
+  products_report
+  padding
+end
+
+def print_sales_report
+  print_header sales_report
+  padding
+  product_sales_report
+  padding
+end
+
+def print_brands_report
+  padding
+  brands_report
+end
 
 def start
   setup
   write_file "DATE",Time.now.strftime("%m/%d/%Y")
-  print_header PRODUCT
-  padding
-  products_report
-  padding
-  print_header BRAND
-  padding
-  brands_report
+  print_product_report
+  print_sales_report
+  print_header brand
+  print_brands_report
 end
 
 def product_selling_price (purchases)
@@ -51,30 +64,25 @@ def product_selling_price (purchases)
 end
 
 def separator
-  $report_file.write("---------------------------------------"+"\n")
+  $report_file.puts("*"*20)
 end
 
 def padding
   $report_file.write(""+"\n")
 end
 
-
 def total_price(prices)
-  total_price = 0
-  prices.each { |price| total_price += price }
-  total_price
+  prices.inject(:+)
 end
 
 def total_revenue(revenues)
-  total_revenue = 0
-  revenues.each { |revenue| total_revenue += revenue }
-  total_revenue
+  revenues.inject(:+)
 end
 
 def get_revenue (items,brand)
   revenues = []
   items.each do |item|
-      item["purchases"].each {|purchase| revenues << (purchase["price"]).to_f} if brand?(brand, item)
+    item["purchases"].each {|purchase| revenues << (purchase["price"]).to_f} if brand?(brand, item)
   end
   revenues
 end
@@ -119,26 +127,28 @@ def print_header(data)
 end
 
 def grand_selling_price(selling_price)
-  total_selling_price = 0
-  selling_price.each { |price| total_selling_price+=price }
-  total_selling_price
+  selling_price.inject(:+)
 end
 
-def product_sales_report(item)
-  write_file "Total Purchases", item["purchases"].size
-  selling_price = product_selling_price(item["purchases"])
-  write_file "Total Amount of Sales for #{item["title"]}", grand_selling_price(product_selling_price(item["purchases"]))
-  write_file "Average Selling Price", (grand_selling_price(selling_price)/selling_price.size)
-  write_file "Average Discounted Percentage:", discount_percentage(item["full-price"].to_f-((grand_selling_price(selling_price)/selling_price.size)), item["full-price"].to_f).to_s + " %"
-  separator
+def product_sales_report()
+  padding
+  $products_hash["items"].each do |item|
+    write_file "Total Purchases for #{item["title"]}", item["purchases"].size
+    selling_price = product_selling_price(item["purchases"])
+    write_file "Total Amount of Sales for #{item["title"]}", grand_selling_price(product_selling_price(item["purchases"]))
+    write_file "Average Selling Price", (grand_selling_price(selling_price)/selling_price.size)
+    write_file "Average Discounted Percentage:", discount_percentage(item["full-price"].to_f-((grand_selling_price(selling_price)/selling_price.size)), item["full-price"].to_f).to_s + " %"
+    separator
+  end
 end
 
 def products_report
+  padding
   $products_hash["items"].each do |item|
     write_file "Title",item["title"]
     write_file "Full Price",item["full-price"]
     write_file "Brand",item["brand"]
-    product_sales_report(item)
+    separator
   end
 end
 
@@ -151,6 +161,7 @@ def brand_sales_report(brand, prices, revenues, stock)
 end
 
 def brands_report
+  padding
   $brands.each do |brand|
     write_file "Brand",brand
     revenues = get_revenue($products_hash["items"], brand)
@@ -162,61 +173,3 @@ def brands_report
 end
 
 start
-
-
-# For each product in the data set:
-# Print the name of the toy
-# Print the retail price of the toy
-# Print the name of the brand
-# Calculate and print the total number of purchases
-# Calculate and print the total amount of sales
-# Calculate and print the average price the toy sold for
-# Calculate and print the average discount based off the average sales price
-
-
-
-
-
-
-
-
-# For each brand in the data set:
-# Print the name of the brand
-# Count and print the number of the brand's toys we stock
-# Calculate and print the average price of the brand's toys
-# Calculate and print the total revenue of all the brand's toy sales combined
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Print "Sales Report" in ascii art
-
-# Print today's date
-
-# Print "Products" in ascii art
-
-# For each product in the data set:
-	# Print the name of the toy
-	# Print the retail price of the toy
-	# Calculate and print the total number of purchases
-  # Calculate and print the total amount of sales
-  # Calculate and print the average price the toy sold for
-  # Calculate and print the average discount based off the average sales price
-
-# Print "Brands" in ascii art
-
-# For each brand in the data set:
-	# Print the name of the brand
-	# Count and print the number of the brand's toys we stock
-	# Calculate and print the average price of the brand's toys
-	# Calculate and print the total sales volume of all the brand's toys combined
